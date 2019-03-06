9f2a71719f6843be882fcb4a87bbed23b14d06d9
Nick Brown <nicholas.g.brown@gmail.com>
2019-03-05 20:33:39 -0500

    Rewrote solution using lambdas

--- java/problem_1/MultiplesOf3And5.java
+++ java/problem_1/MultiplesOf3And5.java
@@ -1,11 +1,9 @@
+import java.util.stream.IntStream;
+
 public class MultiplesOf3And5 {
     public static void main(String[] args) {
-        int sum = 0;
-
-        for(int i = 1; i < 1000; i++) {
-            if ((i%3 == 0) || (i%5 == 0))   
-                sum += i;
-        }
+        
+        int sum = IntStream.range(1, 1000).filter(num -> num%3 == 0 || num%5 == 0).sum();
 
         System.out.println("Sum of multiples of 3 and 5 less than 1000: " + sum);
     }
