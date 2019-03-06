a5a7f96ebca148681be3fae1b298fe7a141b364d
Nick Brown <nicholas.g.brown@gmail.com>
2019-03-05 20:16:10 -0500

    Completed problem 1 in Java

---
+++ java/problem_1/MultiplesOf3And5.java
@@ -0,0 +1,12 @@
+public class MultiplesOf3And5 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for(int i = 1; i < 1000; i++) {
+            if ((i%3 == 0) || (i%5 == 0))   
+                sum += i;
+        }
+
+        System.out.println("Sum of multiples of 3 and 5 less than 1000: " + sum);
+    }
+}
