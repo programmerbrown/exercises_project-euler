49bc4d51cef21e1ef21d89466fed591376ce19e1
Nick Brown <nicholas.g.brown@gmail.com>
2019-03-07 19:03:56 -0500

    Completed problem 3 in Project Euler

---
+++ java/problem_3/LargestPrimeFactor.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class LargestPrimeFactor {
+    public static void main(String[] args) {
+        long numberToFactor = 600851475143L;
+        ArrayList<Long> factors = new ArrayList<>();
+
+        while (numberToFactor%2L == 0) {
+            System.out.println("Adding a 2 factor: " + 2);
+            factors.add(2L);
+            numberToFactor /= 2L;
+        }
+
+        for(long i=3L; i <= Math.sqrt(numberToFactor); i+=2L) {
+            while(numberToFactor%i == 0) {
+                factors.add(i);
+                System.out.println("Added factor: " + i);
+                numberToFactor /= i;
+            }
+        }
+
+        if (numberToFactor > 2L) { 
+            System.out.println("Last factor: " + numberToFactor);
+            factors.add(numberToFactor);
+        }
+
+        for(int i=0; i < factors.size(); i++) {
+            System.out.println("num " + i + " " + factors.get(i));
+        }
+    }
+}
